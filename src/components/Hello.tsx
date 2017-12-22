00de98907debbdb67d633277bf9eaed129880725
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-12-22 14:41:57 -0800

    Added Hello component

---
+++ src/components/Hello.tsx
@@ -0,0 +1,46 @@
+
+import * as React from 'react';
+import './Hello.css';
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+// function Hello({ name, enthusiasmLevel = 1 }: Props) {
+//   if (enthusiasmLevel <= 0) {
+//     throw new Error('You could be a little more enthusiastic. :D');
+//   }
+
+//   return (
+//     <div className="hello">
+//       <div className="greeting">
+//         Hello {name + getExclamationMarks(enthusiasmLevel)}
+//       </div>
+//     </div>
+//   );
+// }
+
+class Hello extends React.Component<Props, object> {
+    render() {
+      const { name, enthusiasmLevel = 1 } = this.props;
+  
+      if (enthusiasmLevel <= 0) {
+        throw new Error('You could be a little more enthusiastic. :D');
+      }
+  
+      return (
+        <div className="hello">
+          <div className="greeting">
+            Hello {name + getExclamationMarks(enthusiasmLevel)}
+          </div>
+        </div>
+      );
+    }
+  }
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
+
+export default Hello;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Hello from './components/Hello';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const reactRoot: HTMLElement = document.getElementById('reactRoot') as HTMLElement;
-const reactComponent: JSX.Element = <App />;
+// const reactComponent: JSX.Element = <App />;
+const reactComponent: JSX.Element = <Hello name="TypeScript" enthusiasmLevel={10} />;
 
 ReactDOM.render(reactComponent, reactRoot);
 
